d6196f9f1c8083922f8ccf6fabe4314cdecbfb60
Andrew Post <arpost@emory.edu>
2016-08-04 00:17:40 -0400

    Initial commit.

---
+++ src/main/java/org/eurekaclinical/useragreement/client/EurekaClinicalUserAgreementClient.java
@@ -0,0 +1,54 @@
+package org.eurekaclinical.useragreement.client;
+
+/*-
+ * #%L
+ * Eureka! Clinical User Agreement Client
+ * %%
+ * Copyright (C) 2016 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.net.URI;
+import org.eurekaclinical.common.comm.clients.ClientException;
+import org.eurekaclinical.common.comm.clients.EurekaClinicalClient;
+import org.eurekaclinical.useragreement.client.comm.UserAgreementStatus;
+
+/**
+ *
+ * @author Andrew Post
+ */
+public final class EurekaClinicalUserAgreementClient extends EurekaClinicalClient {
+
+    private final String serviceUrl;
+
+    public EurekaClinicalUserAgreementClient(String inUserAgreementUrl) {
+        super(null);
+        this.serviceUrl = inUserAgreementUrl;
+    }
+
+    @Override
+    protected String getResourceUrl() {
+        return this.serviceUrl;
+    }
+    
+    public UserAgreementStatus getUserAgreementStatus() throws ClientException {
+        return doGet("/proxy-resource/useragreementstatuses/me", UserAgreementStatus.class);
+    }
+    
+    public Long submitUserAgreement(UserAgreementStatus userAgreementStatus) throws ClientException {
+        URI uri = doPostCreate("/proxy-resource/useragreementstatuses", userAgreementStatus);
+        return extractId(uri);
+    }
+}
---
+++ src/main/java/org/eurekaclinical/useragreement/client/comm/UserAgreement.java
@@ -0,0 +1,71 @@
+package org.eurekaclinical.useragreement.client.comm;
+
+import java.util.Date;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/*-
+ * #%L
+ * Eureka! Clinical User Agreement Service
+ * %%
+ * Copyright (C) 2016 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+/**
+ *
+ * @author Andrew Post
+ */
+public class UserAgreement {
+    private Long id;
+    
+    private String text;
+    
+    private Date effectiveAt;
+    
+    private Date expiredAt;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getEffectiveAt() {
+        return effectiveAt;
+    }
+
+    public void setEffectiveAt(Date effectiveAt) {
+        this.effectiveAt = effectiveAt;
+    }
+
+    public Date getExpiredAt() {
+        return expiredAt;
+    }
+
+    public void setExpiredAt(Date expiredAt) {
+        this.expiredAt = expiredAt;
+    }
+    
+}
